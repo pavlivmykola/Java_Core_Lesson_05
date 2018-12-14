895ef2851f87c9cf27973ec8e454333eb46dd438
Pavliv Mykola <mpavliv@ukr.net>
2018-12-14 10:48:51 +0200

    fixed

--- lesson5/src/ua/lviv/lgs/task5/Main.java
+++ lesson5/src/ua/lviv/lgs/task5/Main.java
@@ -2,11 +2,12 @@ package ua.lviv.lgs.task5;
 
 public class Main {
 	public static void main(String[] args) {
-		Frog f = new Frog();
-		f.eat();
-		f.sleep();
-		f.swim();
-		f.walk();
+		Amphibia am = new Frog();
+		Frog frog = (Frog) am;
+		frog.eat();
+		frog.sleep();
+		frog.swim();
+		frog.walk();
 	}
 
 }
